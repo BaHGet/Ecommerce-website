c66c89b3a0e8d2cbca17a86060b9c0ff167e0979
BaHGet <abosama150@gmail.com>
2024-04-22 17:19:46 +0200

    repo for test task peoject

---
+++ src/Components/Header/header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Cart from './cart'
+
+const Header = () => {
+  return (
+    <header>
+      Sections
+      logo
+      <Cart />
+    </header>
+  )
+}
+
+export default Header
