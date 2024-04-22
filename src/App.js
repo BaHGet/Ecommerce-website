5221204cc47c3be1a2cdfb8841d18f7ab4b18659
BaHGet <abosama150@gmail.com>
2024-04-22 20:14:34 +0200

    transfer all rafce -> rce

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import {React, Component} from 'react'
 import './App.css';
 import Header from './Components/Navigation/header.jsx';
 import Category from './Components/categoryPage/category.jsx';
 
-function App() {
-  return (
-    <>
-      <Header />
-      <Category />
-    </>
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <Category />
+      </>
+    );
+  }
 }
 
 export default App;
