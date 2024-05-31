7fe2dfd5d843bc57da2d2e705131e8aaf8e021df
BaHGet <abosama150@gmail.com>
2024-05-31 03:31:17 +0300

    Merge react-graphql into master @547fdf2

--- backend/bootstrap.php
+++ backend/bootstrap.php
@@ -1,5 +1,8 @@
 <?php
 header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: http://localhost:3000');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE');
+header('Access-Control-Allow-Headers: Content-Type, x-requested-with');
 
 require __DIR__ . '/vendor/autoload.php';
 
